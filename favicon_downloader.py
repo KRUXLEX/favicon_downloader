eb1c5f524bbe9216e318a451e88c202c6ea2c9cc
Axel <axel.gocan@grupawp.pl>
2024-02-15 16:34:43 +0100

    ArgParser add

--- favicon_downloader.py
+++ favicon_downloader.py
@@ -3,6 +3,7 @@
 import sys
 import bs4
 import requests
+import argparse
 from urllib.parse import urlparse, urlunparse
 
 def find_icon(domain):
@@ -29,11 +30,24 @@ def download(domain, icon_url):
         out.write(resp.content)
 
 if __name__=='__main__':
-    if len(sys.argv)<2:
-        print('Need txt file!')
-        sys.exit()
-    sites = sys.argv[1]
-    with open(sites, 'r') as sites:
-        for d in sites.readlines():
-            d = d.strip()
-            download(d, find_icon(d))
+    # Set up the command-line argument parser
+    parser = argparse.ArgumentParser(description="Download favicon form remote sites.")
+    parser.add_argument('-f', '--file', help="Path to the input file", type=argparse.FileType('r'))
+    parser.add_argument("domain", nargs='?', help="Enter domain name form favicon will be downloaded")
+
+    # Parse the arguments
+    sites = parser.parse_args()
+    # print(sites.file.name)
+    # print(sites.domain)
+    # if len(sys.argv)<2:
+    #     print('Need txt file!')
+    #     sys.exit()
+    # sites = sys.argv[1]
+    
+    if not sites.file:
+        download(sites.domain, find_icon(sites.domain))
+    else:    
+        with open(sites.file.name, 'r') as sites:
+            for d in sites.readlines():
+                d = d.strip()
+                download(d, find_icon(d))
